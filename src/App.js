feb21dbf19398e3e75a236054d0e5a8e1d3101f4
MartiniLucas <martini.lucas67@gmail.com>
2020-06-14 10:24:07 -0300

    feat: Primeiro Commit

--- src/App.js
+++ src/App.js
@@ -12,11 +12,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://fjn.edu.br"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          React Site
         </a>
       </header>
     </div>
